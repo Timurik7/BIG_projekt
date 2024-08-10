6eb15151ac8709e4684f084d097cb3c1d3793938
Timur <timurskuratovskij@gmail.com>
2024-08-10 13:11:23 +0300

    added player texture

--- setings.py
+++ setings.py
@@ -3,11 +3,12 @@ import pygame
 pygame.init()
 
 win_width = 700
-win_heidth = 500
-fps = 20
+win_height = 500
+FPS = 20
 
-win = pygame.display.set_mode((win_width, win_heidth))
+win = pygame.display.set_mode((win_width, win_height))
 clock = pygame.time.Clock()
 
+player_image = 'textures/player.png'
 
 background_image = pygame.transform.scale(pygame.image.load("textures/background.png"), (700, 500))
